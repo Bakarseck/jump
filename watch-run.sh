98ca866bcf5237a0118fde1e010fda71447732d7
serignmbaye <barsaliou1998@gmail.com>
2023-08-01 18:19:32 +0000

    feat: add check interval flag + efficient way for killing children process

--- watch-run.sh
+++ watch-run.sh
@@ -5,8 +5,7 @@ run_command() {
     echo "Running command: $@"  # Print the command being executed
     "$@" &                     # Execute the command in the background
     sleep 2                    # Sleep for a short interval to allow the command to start
-    # PID=$(pgrep -P $$)         # Find the process ID of the command
-    PID=$(ps aux | grep "/tmp/go" | grep -v grep | awk '{print $2}')  # Find the process ID of the command
+    PID=$(pgrep -P $$)         # Find the process ID of the command
     echo "Command PID: $PID"   # Print the process ID of the command
 }
 
@@ -14,15 +13,27 @@ run_command() {
 COMMAND="go run ."
 DIRECTORY="./"
 EXCLUDE=""
-RESTRICT="*.go"
+RESTRICT="*"
+CHECK_INTERVAL=1
+
+# Function to stop the running command gracefully
+stop_command() {
+    if [ -n "$PID" ]; then
+        echo "Stopping previous command..."
+        pkill -P $PID 2>/dev/null  # Send SIGTERM to stop the previous command children process
+        kill -TERM $PID 2>/dev/null  # Send SIGTERM to stop the previous command gracefully
+        wait $PID 2>/dev/null  # Wait for the command to finish
+    fi
+}
 
 # Process command-line options
-while getopts "c:d:e:p:" opt; do
+while getopts "c:d:e:p:i" opt; do
   case $opt in
     c) COMMAND="$OPTARG" ;;
     d) DIRECTORY="$OPTARG" ;;
     e) EXCLUDE="$OPTARG" ;;
     p) RESTRICT="$OPTARG" ;;
+    i) CHECK_INTERVAL="$OPTARG" ;;
     \?) echo "Invalid option: -$OPTARG" >&2; exit 1 ;;
   esac
 done
@@ -34,13 +45,12 @@ LAST_RUN_TIME=$(date +%T)  # Set the initial last run time
 while true; do
     CHANGED_FILES=$(find $DIRECTORY -type f -newermt "$LAST_RUN_TIME" ! -name "$EXCLUDE" -name "$RESTRICT" 2>/dev/null)  # Find files changed since the last run time, excluding the specified template and matching the specified pattern
 
-    if [[ -n "$CHANGED_FILES" ]]; then
+   if [[ -n "$CHANGED_FILES" ]]; then
         echo "Detected file changes."
-        echo "Stopping previous command..."
-        kill -9 $PID 2>/dev/null  # Stop the previous command by killing its process
+        stop_command  # Stop the previous command gracefully
         run_command $COMMAND  # Run the new command
     fi
 
     LAST_RUN_TIME=$(date +%T)  # Update the last run time
-    sleep 1  # Sleep for a short interval before checking for file changes again
+    sleep "$CHECK_INTERVAL"  # Sleep for a short interval before checking for file changes again
 done
